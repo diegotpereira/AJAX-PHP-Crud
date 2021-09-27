bff6596dc35dbf3af0e30f0932f5ac4aadfd8394
Diego <diegoteixeirapereira@hotmail.com>
2021-09-27 09:59:50 -0300

    alteração nos arquivos php.

--- backend/operacoes.php
+++ backend/operacoes.php
@@ -22,4 +22,55 @@
             mysqli_close($conexao);
         }
     }
+    if (count($_POST) > 0) {
+        # code...
+        if ($_POST['type'] == 2) {
+            # code...
+            $id = $_POST['id'];
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $telefone = $_POST['telefone'];
+            $cidade = $_POST['cidade'];
+            $SQL = "UPDATE `tb_funcionario` SET `nome` = '$nome', `email` = '$email', `telefone` = '$telefone', `cidade` = '$cidade' WHERE id = $id";
+            if (mysqli_query($conexao, $SQL)) {
+                # code...
+                echo json_encode(array("statusCode" => 200));
+            }else {
+                # code...
+                echo "Error: " .$SQL. "<br>" .mysqli_error($conexao);
+            }
+            mysqli_close($conexao);
+        }
+    }
+    if (count($_POST) > 0) {
+        # code...
+        if ($_POST['type'] == 3) {
+            # code...
+            $id = $_POST['id'];
+            $SQL = "DELETE FROM `tb_funcionario` WHERE id = $id";
+            if (mysqli_query($conexao, $SQL)) {
+                # code...
+                echo $id;
+            } else {
+                # code...
+                echo "Error: " .$SQL . "<br>" .mysqli_error($conexao);
+            }
+            mysqli_close($conexao);
+        }
+    }
+    if (count($_POST) > 0) {
+        # code...
+        if ($_POST['type'] == 4) {
+            # code...
+            $id = $_POST['id'];
+            $SQL = "DELETE FROM tb_funcionario WHERE id in ($id)";
+            if (mysqli_query($conexao, $SQL)) {
+                # code...
+                echo $id;
+            }else {
+                echo "Error: " .$SQL . "<br>" .mysqli_error($conexao);
+            }
+            mysqli_close($conexao);
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -14,9 +14,10 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<!-- <link rel="stylesheet" href="css/style.css"> -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/mascara.js"></script>
 
     <script src="ajax/ajax.js"></script>
 </head>
@@ -27,13 +28,13 @@
             <div class="table-title">
                 <div class="row">
                     <div class="col-sm-6">
-                        <h2>Gerenciar<b>Usuários</b></h2>
+                        <h2><b>Gerenciar Usuários</b></h2>
                     </div>
                     <div class="col-sm-6">
                         <a href="#addFuncModal" class="btn btn-success" data-toggle="modal">
                             <i class="material-icons"></i><span>Adicionar Usuário</span>
                         </a>
-                        <a href="JavaScript:void(0)" class="btn btn-danger" id="delete_multiple">
+                        <a href="JavaScript:void(0)" class="btn btn-danger" id="deletar_multiplos">
                             <i class="material-icons"></i><span>Deletar</span>
                         </a>
                     </div>
@@ -44,8 +45,8 @@
                     <tr>
                         <th>
                             <span class="custom-checkbox">
-                                <input type="checkbox" id="selectAll">
-                                <label for="selectAll"></label>
+                                <input type="checkbox" id="selecionarTodos">
+                                <label for="selecionarTodos"></label>
                             </span>
                         </th>
                         <th>ID Nº</th>
@@ -65,7 +66,7 @@
                     <tr id="<?php echo ["id"]; ?>">
                        <td>
                            <span class="custom-checkbox">
-                               <input type="checkbox" class="user_ckeckbox" data-user-id="<?php echo $row["id"]; ?>">
+                               <input type="checkbox" class="usuario_checkbox" data-usuario-id="<?php echo $row["id"]; ?>">
                                <label for="checkbox2"></label>
                            </span>
                        </td>
@@ -76,7 +77,7 @@
                        <td><?php echo $row['cidade']; ?></td>
                        <td>
                            <a href="#editFuncModal" class="edit" data-toggle="modal">
-                               <i class="material-icons update" data-toggle="tooltip" 
+                               <i class="material-icons atualizar" data-toggle="tooltip" 
                                data-id ="<?php echo $row["id"]; ?>"
                                data-nome ="<?php echo $row["nome"]; ?>"
                                data-email ="<?php echo $row["email"]; ?>"
@@ -128,6 +129,66 @@
                         <input type="hidden" value="1" name="type">
                         <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
                         <button type="button" class="btn btn-success" id="btn-add">Adicionar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Editar HTML -->
+    <div id="editFuncModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form id="atualizar_form">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Editar Usuário</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="id_u" name="id" class="form-control" required>
+                        <div class="form-group">
+                            <label>Nome</label>
+                            <input type="text" id="nome_u" name="nome" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" id="email_u" name="email" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Telefone</label>
+                            <input type="phone" id="telefone_u" name="telefone" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Cidade</label>
+                            <input type="city" id="cidade_u" name="cidade" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="hidden" value="2" name="type">
+                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
+                        <button type="button" class="btn btn-info" id="atualizar">Atualizar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Delete HTML -->
+    <div id="deletarFuncModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-header">
+                        <h4 class="modal-title">Deletar Usuário</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="id_d" name="id" class="form-control">
+                        <p>Tem certeza de que deseja excluir estes registros?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
+                        <button type="button" class="btn btn-danger" id="deletar">Deletar</button>
                     </div>
                 </form>
             </div>
